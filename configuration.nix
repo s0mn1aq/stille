46b10e25b6f57d387f5cb03817ceb705a6113e5c
s0mn1aq <somniaq@tutanota.com>
2026-08-14 11:48:54 +0300

    Update configuration.nix skeleton with customized settings

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,26 @@
+{ config, pkgs, ... }:
 
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+
+  networking.hostName = "field";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Minsk";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  environment.systemPackages = with pkgs; [
+    # Add packages here
+  ];
+
+  services.openssh.enable = true;
+
+  users.users.root.initialPassword = "changeme";
+
+  system.stateVersion = "26.05";
+}
