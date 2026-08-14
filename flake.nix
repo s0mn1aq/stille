bfe672311c6b08be80d72a2402c9f380ce3d452d
s0mn1aq <somniaq@tutanota.com>
2026-08-14 16:41:19 +0300

    Refactor flake.nix input and output structure

--- flake.nix
+++ flake.nix
@@ -1,32 +1,33 @@
 {
   description = "queen of the meadow";
 
-#|-------|
-#| input |
-#|-------|
-    inputs = {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-      noctalia = {
-        url = "github:noctalia-dev/noctalia-shell";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-    };
-      noctalia-greeter = {
-        url = "github:noctalia-dev/noctalia-greeter";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+  #|-------|
+  #| input |
+  #|-------|
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    noctalia = {
+      url = "github:noctalia-dev/noctalia";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-#|--------|
-#| output |
-#|--------|
-    outputs = { self, nixpkgs, ... }@inputs: {
-      nixosConfigurations.field = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./configuration.nix
-        ];
-      };
+    noctalia-greeter = {
+      url = "github:noctalia-dev/noctalia-greeter";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-  }
+  };
+
+  #|--------|
+  #| output |
+  #|--------|
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.field = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
