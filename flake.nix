e85074f1569a888497eabd253ce608ff0d1e2601
s0mn1aq <somniaq@tutanota.com>
2026-08-14 16:33:53 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,24 +4,24 @@
 #|-------|
 #| input |
 #|-------|
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+      noctalia = {
+        url = "github:noctalia-dev/noctalia-shell";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
     };
-  };
 
 #|--------|
 #| output |
 #|--------|
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.field = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./configuration.nix
-      ];
+    outputs = { self, nixpkgs, ... }@inputs: {
+      nixosConfigurations.field = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+        ];
+      };
     };
-  };
-}
+  }
